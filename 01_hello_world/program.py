9b0d14e57014a341adbb2c1c46ed68cbb2a0de3d
vadimvshepelev <vadimvshepelev@gmail.com>
2019-01-02 19:03:43 +0300

    first 4 examples added

---
+++ 01_hello_world/program.py
@@ -0,0 +1,13 @@
+print('--------------------------------')
+print('             HELLO APP')
+print('--------------------------------')
+print()
+
+# TODO: This needs cleaned up.
+user_text = input('What is your name? ')
+# TODO: This can be smoother.
+greeting = 'Nice to meet you ' + user_text
+
+print(greeting)
+
+
