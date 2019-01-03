5791b96d88fe294d2795138d9d46766576e32650
vadimvshepelev <vadimvshepelev@gmail.com>
2019-01-03 23:30:29 +0300

    04_journal modified

---
+++ 04_journal/journal.py
@@ -0,0 +1,13 @@
+
+def load(name):
+    # todo: populate from file if it exists.
+    return []
+
+
+def save(name, journal_data):
+    pass
+
+
+def add_entry(text, journal_data):
+    return None
+
